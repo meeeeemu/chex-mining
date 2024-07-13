b14fb9869347884380d154275155d5f9232881f7
mimu <memedream636@gmail.com>
2024-07-13 18:20:59 +0100

    buffed chexaxe recipe and fixed some text

--- js/game/pickaxes/the-chexaxe.mjs
+++ js/game/pickaxes/the-chexaxe.mjs
@@ -1,8 +1,8 @@
 var pickaxeObjectChexaxe = {
     "Name": "Chexaxe",
     "Bonuses": {
-        "Luck": 0.4,
-        "Speed": 3000,
+        "Luck": 0.2,
+        "Speed": 1000,
     },
     "Tier": 2,
     "Equipped": false
@@ -14,9 +14,9 @@ var chexaxeRecipe = {
     "Emerald": {quantity: 2},
     "Diamond": {quantity: 5},
     "Silver": {quantity: 20},
-    "Coal": {quantity: 25},
-    "Tin": {quantity: 35},
-    "Copper": {quantity: 65},
+    "Coal": {quantity: 75},
+    "Tin": {quantity: 105},
+    "Copper": {quantity: 195},
     "Stone": {quantity: 1000}
 }
 
